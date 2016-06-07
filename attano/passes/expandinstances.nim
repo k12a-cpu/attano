25b5647219f5086d64f671dc4c1be0018544ca6c
Kier Davis <kierdavis@gmail.com>
2016-06-07 21:16:02 +0100

    Update expandinstances pass to new syntax

--- attano/passes/expandinstances.nim
+++ attano/passes/expandinstances.nim
@@ -1,15 +1,14 @@
-from strutils import `%`
 import tables
 import attano.types
 
-proc substitute(e: ExprRef, bindings: OrderedTable[NodeName, ExprRef], renames: Table[NodeName, NodeName]): ExprRef =
+proc substitute(e: PExpr, bindings: OrderedTable[NodeName, PExpr], renames: Table[NodeName, NodeName]): PExpr =
   case e.kind
   of exprNodeRef:
     result =
       if e.node in bindings:
         bindings[e.node]
       elif e.node in renames:
-        ExprRef(
+        PExpr(
           loc: e.loc,
           kind: exprNodeRef,
           node: renames[e.node],
@@ -19,23 +18,23 @@ proc substitute(e: ExprRef, bindings: OrderedTable[NodeName, ExprRef], renames: 
   of exprLiteral:
     result = e
   of exprConcat:
-    var newChildren = newSeq[ExprRef](e.concatChildren.len())
+    var newChildren = newSeq[PExpr](e.concatChildren.len())
     for i, child in e.concatChildren:
       newChildren[i] = child.substitute(bindings, renames)
-    result = ExprRef(
+    result = PExpr(
       loc: e.loc,
       kind: exprConcat,
       concatChildren: newChildren,
     )
   of exprMultiply:
-    result = ExprRef(
+    result = PExpr(
       loc: e.loc,
       kind: exprMultiply,
       multiplyCount: e.multiplyCount,
       multiplyChild: e.multiplyChild.substitute(bindings, renames),
     )
   of exprSlice:
-    result = ExprRef(
+    result = PExpr(
       loc: e.loc,
       kind: exprSlice,
       sliceUpperBound: e.sliceUpperBound,
@@ -43,31 +42,62 @@ proc substitute(e: ExprRef, bindings: OrderedTable[NodeName, ExprRef], renames: 
       sliceChild: e.sliceChild.substitute(bindings, renames),
     )
 
-proc instantiate(unit: CompilationUnitRef, loc: Loc, name: string, componentName: string, bindings: OrderedTable[NodeName, ExprRef]) =
-  if componentName in unit.primitives:
-    unit.instances.add(InstanceRef(
-      loc: loc,
-      name: name,
-      componentName: componentName,
-      bindings: bindings,
-    ))
-  elif componentName in unit.composites:
-    let composite = unit.composites[componentName]
-    var renames = initTable[NodeName, NodeName]()
-    for subNode, subNodeWidth in composite.nodeWidths:
-      let newName = "$1.$2" % [name, subNode]
-      renames[subNode] = newName
-      unit.nodeWidths[newName] = subNodeWidth
-    for subInstance in composite.instances:
-      var newBindings = initOrderedTable[NodeName, ExprRef]()
-      for name, value in subInstance.bindings:
-        newBindings[name] = value.substitute(bindings, renames)
-      let newName = "$1.$2" % [name, subInstance.name]
-      instantiate(unit, loc, newName, subInstance.componentName, newBindings)
+proc instantiate(unit: PCompilationUnit, loc: Loc, name: InstanceName, compositeName: CompositeName, bindings: OrderedTable[NodeName, PExpr]) =
+  let compositeDef = unit.composites[compositeName]
+  
+  var renames = initTable[NodeName, NodeName]()
+  for nodeDef in compositeDef.nodes.values():
+    renames[nodeDef.name] = name & "." & nodeDef.name
+  for aliasDef in compositeDef.aliases.values():
+    renames[aliasDef.name] = name & "." & aliasDef.name
+  
+  # Nodes
+  for nodeDef in compositeDef.nodes.values():
+    let newNodeDef = PNodeDef(
+      loc: nodeDef.loc,
+      name: renames[nodeDef.name],
+      width: nodeDef.width,
+    )
+    unit.nodes[newNodeDef.name] = newNodeDef
+  
+  # Aliases
+  for aliasDef in compositeDef.aliases.values():
+    let newAliasDef = PAliasDef(
+      loc: aliasDef.loc,
+      name: renames[aliasDef.name],
+      value: aliasDef.value.substitute(bindings, renames),
+    )
+    unit.aliases[newAliasDef.name] = newAliasDef
+  
+  # Primitives
+  for primitiveDef in compositeDef.primitives.values():
+    var newPinBindings = initOrderedTable[PinNumber, PExpr]()
+    for pin, exp in primitiveDef.pinBindings:
+      newPinBindings[pin] = exp.substitute(bindings, renames)
+    let newPrimitiveDef = PPrimitiveDef(
+      loc: primitiveDef.loc,
+      name: name & "." & primitiveDef.name,
+      device: primitiveDef.device,
+      footprint: primitiveDef.footprint,
+      pinBindings: newPinBindings,
+    )
+    unit.primitives[newPrimitiveDef.name] = newPrimitiveDef
+  
+  # Instances
+  for instanceDef in compositeDef.instances.values():
+    let newInstanceName = name & "." & instanceDef.name
+    var newInstanceBindings = initOrderedTable[NodeName, PExpr]()
+    for name, exp in instanceDef.bindings:
+      newInstanceBindings[name] = exp.substitute(bindings, renames)
+    instantiate(unit, instanceDef.loc, newInstanceName, instanceDef.compositeName, newInstanceBindings)
 
-proc expandInstances*(unit: CompilationUnitRef) =
-  let instances = unit.instances
-  unit.instances = @[]
-  for instance in instances:
-    instantiate(unit, instance.loc, instance.name, instance.componentName, instance.bindings)
-  unit.composites = initOrderedTable[ComponentName, ref Composite]() # empty it, as they are no longer necessary
+proc expandInstances*(unit: PCompilationUnit) =
+  for instance in unit.instances.values():
+    instantiate(unit, instance.loc, instance.name, instance.compositeName, instance.bindings)
+  
+  unit.composites = initOrderedTable[CompositeName, PCompositeDef]()
+  unit.instances = initOrderedTable[InstanceName, PInstanceDef]()
+  
+  # use these instead once we are compiling with Nim v0.14
+  #unit.composites.clear()
+  #unit.instances.clear()
